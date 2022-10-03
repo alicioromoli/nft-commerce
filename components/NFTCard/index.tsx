6140175b9cc2df133c36d6a277b2ba17bd6c09d0
Alicio Romoli <alicioromoli@hotmail.com>
2022-10-03 23:59:18 +0100

    add NTFCard component

--- components/NFTCard/index.tsx
+++ components/NFTCard/index.tsx
@@ -9,15 +9,30 @@ const nft = {
 
 export default function NFTCard() {
   return (
-    <div className="w-24-[24rem] h-[31rem] bg-[#242634] rounded-[1.25rem] cursor-pointer">
+    <div className="w-24-[24rem] h-[31rem] bg-[#242634] rounded-[1.25rem] cursor-pointer hover:scale-105 active:scale-95">
         <div>
             <img src={nft.image} alt={nft.name} className="rounded-[2.25rem] 0-2 w-96 h-80" />
         </div>
 
         <div className="mx-6 mt-4">
             <div className="mb-5">
-                <p>{nft.name}</p>
-                <p>By {nft.author}</p>
+                <p className="text-2xl font-semibold">{nft.name}</p>
+                <p className="text-[#93989A]">By {nft.author}</p>
+            </div>
+
+            <div className="">
+                <p className="text-[#93989A]">Current Bid</p>
+
+                <div className="flex mt-0.5">
+                    {/* Icon ETH */}
+                    <p className="text-xl font-semibold">{nft.price}</p>
+                </div>
+
+                <div className="relative">
+                    <button className="absolute right-1 bottom-0.5 bg-[#ff2748] py-[0.625rem] px-5 rounded-xl hover:scale-105 active:scale-95">
+                        Place a Bid
+                    </button>
+                </div>
             </div>
         </div>
     </div>
