d791c9894db479af3d4f0272ebf0090de43006ef
Alicio Romoli <alicioromoli@hotmail.com>
2022-10-09 21:08:32 +0100

    add router into the nftcard

--- pages/_navbar.tsx
+++ pages/_navbar.tsx
@@ -1,9 +1,16 @@
+import Router from "next/router"
+import { useCallback } from "react"
 import Logo from "../assets/logo"
 
 export default function Navbar() {
+
+  const handleLogoClick = useCallback(() => {
+    Router.push('/')
+  }, [])
+
   return (
     <div>
-      <div className="h-2 px-24 py-10 flex items-center cursor-pointer">
+      <div className="h-2 px-24 py-10 flex items-center cursor-pointer" onClick={handleLogoClick}>
         <Logo />
         <p className="ml-3 font-semibold">NFT Commerce</p>
       </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,18 @@
 import Head from "next/head";
+import { ChangeEvent, useCallback, useMemo, useState } from "react";
+import NFTCard from "../components/NFTCard";
 
 export default function Home() {
+  const [search, setSearch] = useState('')
+
+  const handleSearch = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  }, [])
+
+  const filteredNfts = useMemo(() => {
+    return nfts.filter((nft) => search ? nft.name.toLocaleLowerCase().includes(search.toLocaleLowerCase()) : true)
+  }, [])
+
   return (
     <div className="h-full w-screen bg-[#1d1f2b] px-24">
       <Head>
@@ -10,7 +22,7 @@ export default function Home() {
           <h2 className="text-5xl font-bold mt-24">
             Discover
           </h2>
-          <input className="h-12 w-64 p-4 rounded-xl" type="text" placeholder="Search Item" />
+          <input className="h-12 w-64 p-4 rounded-xl" type="text" placeholder="Search Item" onChange={handleSearch} value={search}/>
       </div>
 
       <hr className="w-full border-[#243634] mt-12"/>
@@ -21,9 +33,44 @@ export default function Home() {
         </h2>
 
         <div className="flex flex-wrap items-start gap-7 mt-7 min-h-[50%]">
-          
+          {nfts.map(nft => <NFTCard nft={nft} key={nft.id}/>)}
         </div>
       </div>
     </div>
   )
 }
+
+export const nfts = [
+  {
+      id: '1',
+      name: '#4473',
+      price: '0.1',
+      author: '0xE51B77159',
+      image: '',
+      description: 'NFT description'
+  },
+  {
+      id: '2',
+      name: '#4474',
+      price: '0.1',
+      author: '0xE51B77159',
+      image: '',
+      description: 'NFT description'
+  },
+  {
+      id: '3',
+      name: '#4475',
+      price: '0.1',
+      author: '0xE51B77159',
+      image: '',
+      description: 'NFT description'
+  },
+  {
+      id: '4',
+      name: '#4476',
+      price: '0.1',
+      author: '0xE51B77159',
+      image: '',
+      description: 'NFT description'
+  }
+]
